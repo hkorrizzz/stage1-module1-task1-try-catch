87f244e5c7a5511a6db7848b77c8db39752ba07a
hkorrizzz <tutvse253@gmail.com>
2024-07-12 13:26:04 +0300

    ff

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -26,8 +26,9 @@ public class ParseIntegers {
                 sum += number;
             }
             catch (NumberFormatException e) {
-                justWords += next;
                 justWords += " ";
+                justWords += next;
+
             }
         }
         System.out.println("Sum is " + sum);
